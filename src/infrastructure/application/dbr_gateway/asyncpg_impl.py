f7e8039785e0671fcfd9a65454213f0e64c90d86
Egor <baseddepartmentzx77@gmail.com>
2023-09-23 19:44:56 +0600

    Add infrastructure for `Login` query handler

--- src/infrastructure/application/dbr_gateway/asyncpg_impl.py
+++ src/infrastructure/application/dbr_gateway/asyncpg_impl.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 from asyncpg import Connection
 
 from src.application.common.query_results import user
+from src.application.common.query_results import auth
 from src.application.common.interfaces.dbr_gateway import DatabaseReadingGateway
 from src.infrastructure.application.utils import as_query_result
 
@@ -16,4 +17,17 @@ class AsyncpgDatabaseReadingGateway(DatabaseReadingGateway):
         data = await self.connection.fetchval(
             "SELECT 1 username_exists FROM users u WHERE u.username = $1 LIMIT 1", username
         )
-        return as_query_result(user.CheckUsernameExists, data)
+        return as_query_result(user.CheckUsernameExists, {"data": data})
+
+    async def login(self, username: str) -> auth.Login:
+        query_result_mapping = await self.connection.fetchrow(
+            """
+            SELECT
+                JSON_BUILD_OBJECT('id', u.id) data,
+                JSON_BUILD_OBJECT('encoded_password', u.encoded_password) extra
+            FROM users u WHERE u.username = $1 LIMIT 1
+            """,
+            username
+        )
+        return as_query_result(auth.Login, query_result_mapping)
+
