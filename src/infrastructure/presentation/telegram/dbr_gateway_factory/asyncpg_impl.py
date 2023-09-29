277638eb679db51c32934abc2121f7f603ad0fdf
Egor <baseddepartmentzx77@gmail.com>
2023-09-29 23:28:46 +0600

    Add dbr_gateway/dbw_gateway factories

---
+++ src/infrastructure/presentation/telegram/dbr_gateway_factory/asyncpg_impl.py
@@ -0,0 +1,19 @@
+from contextlib import asynccontextmanager
+from dataclasses import dataclass
+from typing import AsyncIterator
+
+from asyncpg import Pool
+
+from src.presentation.telegram.common.interfaces.dbr_gateway_factory import DatabaseReadingGatewayFactory
+from src.infrastructure.application.dbr_gateway.asyncpg_impl import AsyncpgDatabaseReadingGateway
+
+
+@dataclass(frozen=True, slots=True)
+class AsyncpgDatabaseReadingGatewayFactory(DatabaseReadingGatewayFactory):
+
+    pool: Pool
+
+    @asynccontextmanager
+    async def create_gateway(self) -> AsyncIterator[AsyncpgDatabaseReadingGateway]:
+        async with self.pool.acquire() as connection:
+            yield AsyncpgDatabaseReadingGateway(connection)
