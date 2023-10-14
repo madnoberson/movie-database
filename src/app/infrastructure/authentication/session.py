0f24b46fe4c2b80df31aec09cc90e934f73b14e4
Egor <baseddepartmentzx77@gmail.com>
2023-10-14 18:50:24 +0600

    Add delete session to `SessionGateway`

--- src/app/infrastructure/authentication/session.py
+++ src/app/infrastructure/authentication/session.py
@@ -32,26 +32,43 @@ class AuthSessionGateway:
         self.redis = redis
         self.expire = expire
 
-    async def save_session(self, session: Session) -> UUID:
-        """Saves session and returns its id"""
-        session_id = self._create_session_id()
-        session_key = f"auth_sessions:{session_id}"
-        session_data = {"user_id": session.user_id.hex}
+    async def save_session(self, session: Session) -> str:
+        """
+        Deletes old sesssion, saves new one and returns its id
+        """
+        await self._delete_session(user_id=session.user_id)
+        session_id = await self._save_session(session)
 
-        await self.redis.hset(name=session_key, mapping=session_data)
-        await self.redis.expire(name=session_key, time=self.expire)
-        
         return session_id
 
     async def get_session(self, session_id: UUID) -> Session:
         """
         Returns session if exists, otherwise raises `SessionDoesNotExistError`
         """
-        data = await self.redis.hgetall(f"auth_sessions:{session_id}")
-        if data is None:
+        data = await self.redis.hgetall(f"auth_sessions:session_id:{session_id}")
+        if not data:
             raise SessionDoesNotExistError()
         return Session(user_id=UUID(data["user_id"]))
     
+    async def _save_session(self, session: Session) -> str:
+        session_id = self._create_session_id()
+        session_key = f"auth_sessions:session_id:{session_id}"
+        session_data = {"user_id": session.user_id.hex}
+
+        await self.redis.set(
+            name=f"auth_sessions:user_id:{session.user_id.hex}",
+            value=session_id, ex=self.expire
+        )
+        await self.redis.hset(name=session_key, mapping=session_data)
+        await self.redis.expire(name=session_key, time=self.expire)
+
+        return session_id
+    
+    async def _delete_session(self, user_id: UUID) -> None:
+        session_id = await self.redis.get(f"auth_sessions:user_id:{user_id.hex}")
+        if session_id is not None:
+            await self.redis.delete(f"auth_sessions:session_id:{session_id}")
+
     def _create_session_id(self) -> str:
         return uuid4().hex
 
--- src/app/presentation/web_api/exceptions.py
+++ src/app/presentation/web_api/exceptions.py
@@ -5,6 +5,7 @@ from fastapi.responses import JSONResponse
 
 from app.application.common.exceptions import authentication as auth_exceptions
 from app.application.common.exceptions import user as user_exceptions
+from app.infrastructure.authentication.session import SessionDoesNotExistError
 
 
 def setup_exception_handlers(app: FastAPI) -> None:
@@ -12,9 +13,10 @@ def setup_exception_handlers(app: FastAPI) -> None:
     app.add_exception_handler(auth_exceptions.PasswordIsNotCorrectError, create_exception_handler(401))
     app.add_exception_handler(user_exceptions.UserAlreadyExistsError, create_exception_handler(409))
     app.add_exception_handler(user_exceptions.UserDoesNotExistError, create_exception_handler(404))
+    app.add_exception_handler(SessionDoesNotExistError, create_exception_handler(401))
 
 
-def create_exception_handler(status_code: int, data: dict[str, Any] = {}) -> Callable:
+def create_exception_handler(status_code: int, data: dict[str, Any] | None = None) -> Callable:
     async def exception_handler(request: Request, exc: Exception) -> JSONResponse:
         return JSONResponse(content=data, status_code=status_code)
     return exception_handler
