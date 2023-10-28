1d3fd41fb4aa71ef15ce10db4d4007913009062b
Egor <baseddepartmentzx77@gmail.com>
2023-10-28 23:33:29 +0600

    [admin] Add `CreateSuperuser` command

---
+++ src/movie-database-admin/app/domain/models/superuser.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+from datetime import datetime
+from uuid import UUID
+
+from app.domain.constants import SuperUserPermissionEnum
+from .model import Model
+
+
+@dataclass(slots=True)
+class Superuser(Model):
+
+    id: UUID
+    username: str
+    email: str
+    password: str
+    is_blocked: bool
+    permissions: list[SuperUserPermissionEnum]
+    created_at: datetime
+
+    @classmethod
+    def create(
+        cls, superuser_id: UUID, username: str, email: str,
+        password: str, permissions: list[SuperUserPermissionEnum],
+        created_at: datetime
+    ) -> "Superuser":
+        return Superuser(
+            id=superuser_id, username=username, email=email,
+            password=password, is_blocked=False, permissions=permissions,
+            created_at=created_at
+        )
