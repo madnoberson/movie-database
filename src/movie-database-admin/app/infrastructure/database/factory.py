13f13440e33c830b6eea042be3e620a36f914cda
Egor <baseddepartmentzx77@gmail.com>
2023-10-29 21:01:47 +0600

    [admin] Add infrastructure for `Login`

--- src/movie-database-admin/app/infrastructure/database/factory.py
+++ src/movie-database-admin/app/infrastructure/database/factory.py
@@ -6,6 +6,7 @@ from asyncpg.connection import Connection
 
 from .uow import RepositoryUnitOfWork
 from . import repositories
+from . import readers
 
 
 class DatabaseFactoryManager:
@@ -22,6 +23,11 @@ class DatabaseFactoryManager:
     async def build_repo_factory(self) -> AsyncIterator["RepositoryFactory"]:
         async with self.repo_connection_pool.acquire() as connection:
             yield RepositoryFactory(connection)
+    
+    @asynccontextmanager
+    async def build_reader_factory(self) -> AsyncIterator["ReaderFactory"]:
+        async with self.reader_connection_pool.acquire() as connection:
+            yield ReaderFactory(connection)
 
 
 class RepositoryFactory:
@@ -36,3 +42,12 @@ class RepositoryFactory:
         transaction = self.connection.transaction()
         await transaction.start()
         return RepositoryUnitOfWork(transaction)
+
+
+class ReaderFactory:
+
+    def __init__(self, connection: Connection) -> None:
+        self.connection = connection
+    
+    def build_auth_reader(self) -> readers.AuthnReaderImpl:
+        return readers.AuthnReaderImpl(self.connection)
---
+++ src/movie-database-admin/app/infrastructure/database/readers/auth.py
@@ -0,0 +1,26 @@
+from asyncpg.connection import Connection
+
+from app.application.common.interfaces.readers import AuthReader
+from app.application.common.query_results import auth as query_results
+from app.infrastructure.database.mappers import as_query_result
+
+
+class AuthnReaderImpl(AuthReader):
+
+    def __init__(self, connection: Connection) -> None:
+        self.connection = connection
+
+    async def login(self, username: str) -> query_results.Login | None:
+        data = await self.connection.fetchrow(
+            """
+            SELECT ROW_TO_JSON(u.*) data, ROW_TO_JSON(u.*) extra
+            FROM superusers su WHERE su.username = $1 LIMIT 1
+            """,
+            username
+        )
+        if data is None: return None
+
+        data = dict(data)
+        data["data"]["superuser_id"] = data["data"]["id"]
+        
+        return as_query_result(query_results.Login, data)
