f2973cab2196162cef670c5e7226e9abd6f7d006
Egor <baseddepartmentzx77@gmail.com>
2023-10-31 00:56:26 +0600

    [admin] Add `EnsureUser` command

---
+++ src/movie-database-admin/app/application/commands/user/ensure_user.py
@@ -0,0 +1,42 @@
+from dataclasses import dataclass
+from datetime import datetime
+from uuid import UUID
+
+from app.domain.models.user import User
+from app.application.common.interfaces.uow import UnitOfWork
+from app.application.common.interfaces import repositories
+from app.application.common.exceptions import user as user_exceptions
+from app.application.commands.handler import CommandHandler
+
+
+@dataclass(frozen=True, slots=True)
+class InputDTO:
+
+    user_id: UUID
+    username: str
+    created_at: datetime
+
+
+class EnsureUser(CommandHandler):
+
+    def __init__(
+        self,
+        user_repo: repositories.UserRepository,
+        uow: UnitOfWork
+    ) -> None:
+        self.user_repo = user_repo
+        self.uow = uow
+    
+    async def __call__(self, data: InputDTO) -> None:
+        # 1.Ensure user doesn't exist
+        if self.user_repo.check_user_exists(username=data.username):
+            raise user_exceptions.UserAlreadyExistsError()
+
+        # 2.Create `User`
+        user = User.create(
+            user_id=data.user_id, username=data.username,
+            created_at=data.created_at
+        )
+        await self.user_repo.save_user(user)
+
+        await self.uow.commit()
