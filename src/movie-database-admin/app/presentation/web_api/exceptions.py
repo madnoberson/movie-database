9d4e9db38146f66fbb0c8dd8834ff259ff7a5d97
Egor <baseddepartmentzx77@gmail.com>
2023-10-31 18:32:37 +0600

    [admin] Add exception handlers to web_api

---
+++ src/movie-database-admin/app/presentation/web_api/exceptions.py
@@ -0,0 +1,23 @@
+from typing import Callable, Any
+
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+
+from app.domain.exceptions import access as access_exceptions
+from app.application.common.exceptions import auth as auth_exceptions
+from app.application.common.exceptions import user as user_exceptions
+from app.infrastructure.authentication.session.session.gateway import SessionDoesNotExistError
+
+
+def setup_exception_handlers(app: FastAPI) -> None:
+    app.add_exception_handler(access_exceptions.AccessDeniedError, create_exception_handler(403))
+    app.add_exception_handler(auth_exceptions.UnauthorizedError, create_exception_handler(401))
+    app.add_exception_handler(auth_exceptions.IncorrectPasswordError, create_exception_handler(401))
+    app.add_exception_handler(user_exceptions.UserAlreadyExistsError, create_exception_handler(409))
+    app.add_exception_handler(SessionDoesNotExistError, create_exception_handler(401))
+
+
+def create_exception_handler(status_code: int, data: dict[str, Any] | None = None) -> Callable:
+    async def exception_handler(request: Request, exc: Exception) -> JSONResponse:
+        return JSONResponse(content=data, status_code=status_code)
+    return exception_handler
