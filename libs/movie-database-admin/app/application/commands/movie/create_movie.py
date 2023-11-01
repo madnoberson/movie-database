98f2d963e33894b6783f1543b5f44f1db6701813
Egor <baseddepartmentzx77@gmail.com>
2023-11-02 03:03:15 +0600

    [admin] Add `CreateMovie` command

--- libs/movie-database-admin/app/application/commands/movie/create_movie.py
+++ libs/movie-database-admin/app/application/commands/movie/create_movie.py
@@ -2,6 +2,8 @@ from dataclasses import dataclass
 from datetime import datetime
 from uuid import UUID, uuid4
 
+from app.domain.models.movie import Movie
+from app.domain.events.movie import MovieCreated
 from app.domain.services.access import AccessService
 from app.application.common.interfaces.identity_provider import IdentityProvider
 from app.application.common.interfaces.event_bus import EventBus
@@ -39,4 +41,24 @@ class CreateMovie(CommandHandler):
         self.uow = uow
     
     async def __call__(self, data: InputDTO) -> None:
-        ...
+        # 1.Ensure current superuser can create movie
+        access_policy = await self.identity_provider.get_access_policy()
+        self.access_service.ensure_can_create_movie(access_policy)
+
+        # 2.Create movie
+        movie = Movie.create(
+            movie_id=uuid4(), en_name=data.en_name,
+            created_at=datetime.utcnow()
+        )
+        await self.movie_repo.save_movie(movie)
+
+        # 3.Publish `MovieCreated` event to event bus
+        movie_created_event = MovieCreated(
+            movie_id=movie.id, en_name=movie.en_name,
+            created_at=movie.created_at
+        )
+        await self.event_bus.publish(movie_created_event)
+
+        await self.uow.commit()
+
+        return OutputDTO(movie_id=movie.id)
