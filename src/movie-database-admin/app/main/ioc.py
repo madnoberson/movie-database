297bd7c795c76f66e62406e1fb7456435f4f88d2
Egor <baseddepartmentzx77@gmail.com>
2023-11-01 23:58:18 +0600

    [admin] `EnsureSuperuserPasswordChange` to `IoC`

--- src/movie-database-admin/app/main/ioc.py
+++ src/movie-database-admin/app/main/ioc.py
@@ -7,6 +7,7 @@ from app.application.commands.superuser.create_superuser import CreateSuperuser
 from app.application.commands.superuser.change_password import ChangeSuperuserPassword
 from app.application.commands.user.change_username import ChangeUsername
 from app.application.commands.superuser.ensure_superuser import EnsureSuperuser
+from app.application.commands.superuser.ensure_password_change import EnsureSuperuserPasswordChange
 from app.application.commands.user.ensure_user import EnsureUser
 from app.application.commands.user.ensure_username_change import EnsureUsernameChange
 from app.application.queries.auth.login import Login
@@ -72,6 +73,16 @@ class IoC(HandlerFactory):
     async def ensure_superuser(self) -> AsyncIterator[EnsureSuperuser]:
         async with self.db_factory_manager.build_repo_factory() as repo_factory:
             yield EnsureSuperuser(
+                superuser_repo=repo_factory.build_superuser_repo(),
+                uow=UnitOfWorkImpl(await repo_factory.build_uow())
+            )
+    
+    @asynccontextmanager
+    async def ensure_superuser_password_change(
+        self
+    ) -> AsyncIterator[EnsureSuperuserPasswordChange]:
+        async with self.db_factory_manager.build_repo_factory() as repo_factory:
+            yield EnsureSuperuserPasswordChange(
                 superuser_repo=repo_factory.build_superuser_repo(),
                 uow=UnitOfWorkImpl(await repo_factory.build_uow())
             )
--- src/movie-database-admin/app/presentation/handler_factory.py
+++ src/movie-database-admin/app/presentation/handler_factory.py
@@ -6,6 +6,7 @@ from app.application.commands.superuser.create_superuser import CreateSuperuser
 from app.application.commands.superuser.change_password import ChangeSuperuserPassword
 from app.application.commands.user.change_username import ChangeUsername
 from app.application.commands.superuser.ensure_superuser import EnsureSuperuser
+from app.application.commands.superuser.ensure_password_change import EnsureSuperuserPasswordChange
 from app.application.commands.user.ensure_user import EnsureUser
 from app.application.commands.user.ensure_username_change import EnsureUsernameChange
 from app.application.queries.auth.login import Login
@@ -35,6 +36,12 @@ class HandlerFactory(ABC):
     async def ensure_superuser(
         self
     ) -> AsyncContextManager[EnsureSuperuser]:
+        raise NotImplementedError
+    
+    @abstractmethod
+    async def ensure_superuser_password_change(
+        self
+    ) -> AsyncContextManager[EnsureSuperuserPasswordChange]:
         raise NotImplementedError
     
     @abstractmethod
