cf05693b7c8c6e92d3cc3763f314b18d36029caf
Egor <baseddepartmentzx77@gmail.com>
2023-11-07 22:14:31 +0600

    [movie-database] Add `RerateMovie` command

--- libs/movie-database/src/app/domain/services/movie_rating.py
+++ libs/movie-database/src/app/domain/services/movie_rating.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from app.domain.models.movies_rating_policy import MoviesRatingPolicy
+from app.domain.models.movie_rating import MovieRating
 from app.domain.models.user import User
 
 
@@ -11,6 +12,22 @@ class MovieRatingService:
         movies_rating_policy: MoviesRatingPolicy
     ) -> bool:
         if (
+            user.rated_movie_count <
+            movies_rating_policy.required_rated_movie_count
+            or
+            datetime.utcnow() - user.created_at <
+            movies_rating_policy.required_days_pass_after_registration
+        ):
+            return False
+        return True
+
+    def check_user_can_fully_rerate_movie(
+        self, user: User, movie_rating: MovieRating,
+        movies_rating_policy: MoviesRatingPolicy
+    ) -> bool:
+        if (
+            not movie_rating.is_full
+            and
             user.rated_movie_count <
             movies_rating_policy.required_rated_movie_count
             or
