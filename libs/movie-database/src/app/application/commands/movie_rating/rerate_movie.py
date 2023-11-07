62afef0bcb52287f3ee9f6bedcb8b6881d706ef1
Egor <baseddepartmentzx77@gmail.com>
2023-11-07 23:00:53 +0600

    [movie-database] Fix `RerateMovie` command

--- libs/movie-database/src/app/application/commands/movie_rating/rerate_movie.py
+++ libs/movie-database/src/app/application/commands/movie_rating/rerate_movie.py
@@ -25,8 +25,6 @@ class RerateMovie(CommandHandler):
         self,
         movie_rating_repo: repositories.MovieRatingRepository,
         movie_repo: repositories.MovieRepository,
-        user_repo: repositories.UserRepository,
-        movies_rating_policy_repo: repositories.MoviesRatingPolicyRepository,
         event_bus: EventBus,
         identity_provider: IdentityProvider,
         movie_rating_service: MovieRatingService,
@@ -34,8 +32,6 @@ class RerateMovie(CommandHandler):
     ) -> None:
         self.movie_rating_repo = movie_rating_repo
         self.movie_repo = movie_repo
-        self.user_repo = user_repo
-        self.movies_rating_policy_repo = movies_rating_policy_repo
         self.event_bus = event_bus
         self.identity_provider = identity_provider
         self.movie_rating_service = movie_rating_service
@@ -57,32 +53,19 @@ class RerateMovie(CommandHandler):
         movie_rating.update(rating=data.rating, updated_at=datetime.utcnow())
         await self.movie_rating_repo.update_movie_rating(movie_rating)
 
-        # 4.Get user
-        user = await self.user_repo.get_user(user_id=current_user_id)
-
-        # 5.Get movies rating policy
-        movies_rating_policy = (
-            await self.movies_rating_policy_repo.get_movies_rating_policy()
-        )
-
-        # 6.Update movie's rating if movie rating is full
+        # 4.Update movie's rating if movie rating is full
         if self.movie_rating_service.check_user_can_fully_rerate_movie(
-            user=user, movie_rating=movie_rating,
-            movies_rating_policy=movies_rating_policy
+            movie_rating=movie_rating
         ):
-            # 6.1.Get movie
+            # 4.1.Get movie
             movie = await self.movie_repo.get_movie(movie_id=data.movie_id)
 
-            # 6.2.Update movie's rating
+            # 4.2.Update movie's rating
             movie.remove_user_rating(user_rating=old_movie_rating)
             movie.add_user_rating(user_rating=data.rating)
             await self.movie_repo.update_movie(movie)
 
-        # 7.Update user rated movies count
-        user.add_movie_rating()
-        await self.user_repo.update_user(user)
-
-        # 8.Publish `MovieRerated` event to event bus
+        # 5.Publish `MovieRerated` event to event bus
         movie_rated_event = MovieRerated(
             user_id=movie_rating.user_id, movie_id=movie_rating.movie_id,
             rating=movie_rating.rating, is_full=movie_rating.is_full,
