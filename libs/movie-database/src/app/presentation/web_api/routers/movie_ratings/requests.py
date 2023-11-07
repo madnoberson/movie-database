0a77c58e2898657943ac1879c7100b7fe48dea83
Egor <baseddepartmentzx77@gmail.com>
2023-11-08 00:41:18 +0600

    [movie-database] Refactor `movie_rating` schmeas

--- libs/movie-database/src/app/presentation/web_api/routers/movie_ratings/requests.py
+++ libs/movie-database/src/app/presentation/web_api/routers/movie_ratings/requests.py
@@ -1,11 +1,25 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
 
 
 class RateMovieSchema(BaseModel):
 
     rating: float = Field(ge=0.5, le=10)
 
+    @field_validator("rating")
+    @classmethod
+    def validate_rating(cls, rating: float) -> float:
+        if rating % 0.5 != 0:
+            raise ValueError()
+        return rating
+
 
 class RerateMovieSchema(BaseModel):
 
     rating: float = Field(ge=0.5, le=10)
+
+    @field_validator("rating")
+    @classmethod
+    def validate_rating(cls, rating: float) -> float:
+        if rating % 0.5 != 0:
+            raise ValueError()
+        return rating
