cb39d9515cdac60d0ab3d35f1cd347b7c408ae32
Egor <baseddepartmentzx77@gmail.com>
2023-11-07 13:55:41 +0600

    [movie-database] Add new exception handlers

--- libs/movie-database/app/presentation/web_api/exceptions.py
+++ libs/movie-database/app/presentation/web_api/exceptions.py
@@ -5,6 +5,8 @@ from fastapi.responses import JSONResponse
 
 from app.application.common.exceptions import auth as auth_exceptions
 from app.application.common.exceptions import user as user_exceptions
+from app.application.common.exceptions import movie as movie_exceptions
+from app.application.common.exceptions import movie_rating as movie_rating_exceptions
 from app.infrastructure.authentication.session.session_gateway import SessionDoesNotExistError
 
 
@@ -13,6 +15,10 @@ def setup_exception_handlers(app: FastAPI) -> None:
     app.add_exception_handler(auth_exceptions.IncorrectPasswordError, create_exception_handler(401))
     app.add_exception_handler(user_exceptions.UserAlreadyExistsError, create_exception_handler(409))
     app.add_exception_handler(user_exceptions.UserDoesNotExistError, create_exception_handler(404))
+    app.add_exception_handler(movie_exceptions.MovieAlreadyExistsError, create_exception_handler(409))
+    app.add_exception_handler(movie_exceptions.MovieDoesNotExistError, create_exception_handler(404))
+    app.add_exception_handler(movie_rating_exceptions.MovieRatingAlreadyExistsError, create_exception_handler(409))
+    app.add_exception_handler(movie_rating_exceptions.MovieRatingDoesNotExistError, create_exception_handler(404))
     app.add_exception_handler(SessionDoesNotExistError, create_exception_handler(401))
 
 
