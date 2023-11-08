b74aaed2547c7224b9cbbc45d2eafcf8f71449d0
Egor <baseddepartmentzx77@gmail.com>
2023-11-09 00:41:18 +0600

    [movie-database] Refactor `RateMovie` command

--- libs/movie-database/src/app/application/commands/movie_rating/rate_movie.py
+++ libs/movie-database/src/app/application/commands/movie_rating/rate_movie.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from datetime import datetime
 from uuid import UUID
 
+from app.domain.models.user import User
 from app.domain.models.movie_rating import MovieRating
 from app.domain.models.achievement import Achievement
 from app.domain.events.movie_rating import MovieRated
@@ -53,95 +54,92 @@ class RateMovie(CommandHandler):
         self.uow = uow
     
     async def __call__(self, data: InputDTO) -> None:
-        # 1.Get current user id
         current_user_id = await self.identity_provider.get_current_user_id()
+        current_user = await self.user_repo.get_user(user_id=current_user_id)
 
-        # 2.Ensure movie is not rated by user
+        await self._rate_movie(
+            current_user=current_user, movie_id=data.movie_id,
+            rating=data.rating
+        )
+        await self._give_achievements(current_user=current_user)
+
+        await self.uow.commit()
+    
+    async def _rate_movie(self, current_user: User, movie_id: UUID, rating: float) -> None:
+        # 1.Ensure movie is not rated by user
         if await self.movie_rating_repo.check_movie_rating_exists(
-            user_id=current_user_id, movie_id=data.movie_id
+            user_id=current_user.id, movie_id=movie_id
         ):
             raise movie_rating_exceptions.MovieRatingAlreadyExistsError()
         
-        # 3.Get movie
-        movie = await self.movie_repo.get_movie(movie_id=data.movie_id)
+        # 2.Ensure movie exists
+        movie = await self.movie_repo.get_movie(movie_id=movie_id)
         if movie is None:
             raise movie_exceptions.MovieDoesNotExistError()
-  
-        # 4.Get user
-        user = await self.user_repo.get_user(user_id=current_user_id)
 
-        # 5.Get movies rating policy
+        # 3.Get movies rating policy
         movies_rating_policy = (
             await self.movies_rating_policy_repo.get_movies_rating_policy()
         )
-        
-        # 6.Create full movie rating and update movie rating
-        # if user can fully rate movie, otherwise create not full movie rating
-        if self.movie_rating_service.check_user_can_fully_rate_movie(
-            user=user, movies_rating_policy=movies_rating_policy
-        ):
-            # 6.1.Create movie rating
-            movie_rating = MovieRating.create(
-                user_id=current_user_id, movie_id=movie.id,
-                rating=data.rating, created_at=datetime.utcnow(),
-                is_full=True
-            )
-            await self.movie_rating_repo.save_movie_rating(movie_rating)
 
-            # 6.2 Update movie's rating
-            movie.add_user_rating(user_rating=data.rating)
+        # 4.Update movie's rating if movie rating is 'full'
+        movie_rating_is_full = (
+            self.movie_rating_service.check_user_can_fully_rate_movie(
+                user=current_user, movies_rating_policy=movies_rating_policy
+            )
+        )
+        if movie_rating_is_full:
+            movie.add_user_rating(user_rating=rating)
             await self.movie_repo.update_movie(movie)
-        else:
-            # 6.1.Create movie rating
-            movie_rating = MovieRating.create(
-                user_id=current_user_id, movie_id=movie.id,
-                rating=data.rating, created_at=datetime.utcnow()
-            )
-            await self.movie_rating_repo.save_movie_rating(movie_rating)
 
-        # 7.Update user rated movies count
-        user.add_movie_rating()
-        await self.user_repo.update_user(user)
-
-        # 8.Get rated movies achievements policy
-        rated_movies_achievements_policy = (
-            await self.rated_movies_achievements_policy_repo.
-            get_rated_movies_achievements_policy()
+        # 5.Create movie rating
+        movie_rating = MovieRating.create(
+            user_id=current_user.id, movie_id=movie.id, rating=rating,
+            created_at=datetime.utcnow(), is_full=movie_rating_is_full
         )
+        await self.movie_rating_repo.save_movie_rating(movie_rating)
 
-        # 9.Create achievement and publish `AchievementObtained` event to event bus
-        # if user can obtain rated movies achievement or achievement already exists
-        achievement_type = self.achievement_service.get_rated_movie_achievement_type(
-            user=user,
-            rated_movies_achievements_policy=rated_movies_achievements_policy
-        )
-        if (
-            achievement_type is not None and
-            await self.achievement_repo.check_achievement_exists(
-                user_id=user.id, achievement_type=achievement_type
-            )
-        ):
-            # 9.1.Create achievement
-            achievement = Achievement.create(
-                user_id=user.id, type=achievement_type,
-                created_at=datetime.utcnow()
-            )
-            await self.achievement_repo.save_achievement(achievement)
+        # 6.Update user rated movies count
+        current_user.add_movie_rating()
+        await self.user_repo.update_user(current_user)
 
-            # 9.2.Publish `AchievementObtained` event to event bus
-            achievement_obtained_event = AchievementObtained(
-                user_id=user.id, type=achievement_type,
-                created_at=achievement.created_at
-            )
-            await self.event_bus.publish(achievement_obtained_event)
-
-
-        # 10.Publish `MovieRated` event to event bus
+        # 7.Publish `MovieRated` event to event bus
         movie_rated_event = MovieRated(
             user_id=movie_rating.user_id, movie_id=movie_rating.movie_id,
             rating=movie_rating.rating, is_full=movie_rating.is_full,
             created_at=movie_rating.created_at
         )
         await self.event_bus.publish(movie_rated_event)
+    
+    async def _give_achievements(self, current_user: User) -> None:
+        # 1.Get rated movies achievements policy
+        rated_movies_achievements_policy = (
+            await self.rated_movies_achievements_policy_repo.
+            get_rated_movies_achievements_policy()
+        )
 
-        await self.uow.commit()
+        # 2.Create achievement and publish `AchievementObtained` event to event bus
+        # if user can obtain rated movies achievement and achievement doesn't exist
+        achievement_type = self.achievement_service.get_rated_movie_achievement_type(
+            user=current_user,
+            rated_movies_achievements_policy=rated_movies_achievements_policy
+        )
+        if (
+            achievement_type is not None and
+            await self.achievement_repo.check_achievement_exists(
+                user_id=current_user.id, achievement_type=achievement_type
+            )
+        ):
+            # 2.1.Create achievement
+            achievement = Achievement.create(
+                user_id=current_user.id, type=achievement_type,
+                created_at=datetime.utcnow()
+            )
+            await self.achievement_repo.save_achievement(achievement)
+
+            # 2.2.Publish `AchievementObtained` event to event bus
+            achievement_obtained_event = AchievementObtained(
+                user_id=current_user.id, type=achievement_type,
+                created_at=achievement.created_at
+            )
+            await self.event_bus.publish(achievement_obtained_event)
